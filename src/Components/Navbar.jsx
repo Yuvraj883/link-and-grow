e6d71d4c0cc43e01ecc88690548092c6d7707bfa
Yuvraj <devyuvraj883@gmail.com>
2024-03-21 18:07:59 +0530

    Update style

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,14 +2,13 @@ const Navbar = () => {
   return (
     <>
       <nav className='flex justify-between shadow-md px-10 py-4'>
-        <span className='font-bold font-serif text-lg'>Link&Grow</span>
-
-        <ul className='flex flex-row font-semibold '>
-          <li className='cursor-pointer px-2 mx-2 hover:text-red-900'>Home</li>
-          <li className='cursor-pointer px-2 mx-2 hover:text-red-900'>About</li>
-          <li className='cursor-pointer px-2 mx-2 hover:text-red-900'>Tools</li>
-        </ul>
+        <span className='font-extrabold text-red-700 font-serif text-lg'>Link&Grow</span>
       </nav>
+      <ul className='flex flex-col font-semibold h-[100vh] px-10 py-8 text-lg shadow-md w-[15%]'>
+          <li className='cursor-pointer px-2 py-2  mx-2 hover:text-red-700  border-b-2 border-gray-200'>Home</li>
+          <li className='cursor-pointer px-2 py-2 mx-2 hover:text-red-700 border-b-2 border-gray-200'>About</li>
+          <li className='cursor-pointer px-2 py-2 mx-2 hover:text-red-700 border-b-2 border-gray-200'>Tools</li>
+        </ul>
     </>
   )
 }
