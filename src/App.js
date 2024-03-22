d93c17c6737395bf92f8a30af120c3a5a2346321
Yuvraj <devyuvraj883@gmail.com>
2024-03-23 03:03:09 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-
-import { Button } from '@mui/material'
+// import { Button } from '@mui/material'
 import Navbar from './Components/Navbar'
 import TextContentGeneration from './Pages/TextContentGeneration'
+import PostGeneration from './Pages/PostGeneration'
+import LeftBar from './Components/LeftBar'
 
 function App() {
   return (
     <>
-     <Navbar/>
-     <TextContentGeneration/>
+      <Navbar />
+      <div className='flex'>
+        <LeftBar />
+        <PostGeneration />
+      </div>
 
+      
     </>
   )
 }
